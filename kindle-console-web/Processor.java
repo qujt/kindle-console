b8794ab664394908ce966976090901051f760c3a
huainiannin <huainiannin@gmail.com>
2018-01-07 11:18:47 +0800

    命令执行类

--- kindle-console-web/Processor.java
+++ kindle-console-web/Processor.java
@@ -1,11 +1,33 @@
+import java.io.IOException;
 /*
 *一个用于处理linux命令输入，返回命令输出的类
 *
 */
 public class Processor
 {
-	public static void main(String[] args)
-	{
-		System.out.println("hello world!");
+	/*命令运行实例*/
+	public static Runtime runtime;
+
+	public static Runtime getRuntime() {
+		
+		if(runtime == null)
+		{
+			return runtime = Runtime.getRuntime();	
+		}
+		else
+		{
+			return runtime;
+		}
+	}
+	public static Process executeCMD(String cmd) {
+		Process ps = null;
+		String[] cmds = {"/bin/bash","-c", cmd };
+		try {
+			ps = getRuntime().exec(cmds);
+		} catch (IOException e)
+		{
+			System.out.println("Process running error");
+		}
+		return ps;
 	}
 }
